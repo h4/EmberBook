098350dccc1b1eba4691b5a56c514c83fb0bfa58
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-03-16 03:30:13 +0400

    Changed Index view model

--- js/app.js
+++ js/app.js
@@ -1,18 +1,29 @@
 App = Ember.Application.create();
 
-App.Router.map(function() {
+App.Router.map(function () {
     this.route('about');
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+    model: function () {
+        var questions = [
+            {
+                title: 'How do I feel hamsters?',
+                author: 'Tom Dale'
+            },
+            {
+                title: 'Are humans insane?',
+                author: 'Tomster the Hamster'
+            }
+        ];
+
+        return questions;
+    }
 });
 
-App.IndexController = Ember.Controller.extend({
+App.IndexController = Ember.ArrayController.extend({
     siteTitle: 'Welcome to EmberOverflow',
-    currentTime: function() {
+    currentTime: function () {
         return (new Date);
     }.property()
 });
