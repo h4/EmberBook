c8e06868c4b149af39b323d937804333a28db5ef
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-03-17 01:09:33 +0400

    Don't show login form to logged-in user

--- js/controllers/sign_in_controller.js
+++ js/controllers/sign_in_controller.js
@@ -1,6 +1,7 @@
 "use strict";
 
 App.SignInController = Ember.Controller.extend({
+    needs: ['application'],
     actions: {
         signIn: function() {
             var email = this.get('email');
