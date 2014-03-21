05e7b7ff291d2afb18ad0429b7ff6d486dd153f4
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-03-22 00:57:23 +0400

    Show count of answers

---
+++ js/components/question_component.js
@@ -0,0 +1,7 @@
+"use strict";
+
+App.QuestionPreviewComponent = Ember.Component.extend({
+    answersCount: function() {
+        return this.get('question.answers.length');
+    }.property('question.answers.length')
+});
