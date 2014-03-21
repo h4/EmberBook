5682fc766bf075082654081213257a71932f3d42
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-03-22 00:13:04 +0400

    Added controller that can save answers

--- js/controllers/question_controllers.js
+++ js/controllers/question_controllers.js
@@ -1,5 +1,30 @@
 "use strict";
 
+App.QuestionController = Ember.ObjectController.extend({
+    needs: ['application'],
+    actions: {
+        answerQuestion: function() {
+            var controller = this;
+            var answer = this.store.createRecord('answer', {
+                answer: this.get('answer'),
+                question: this.get('model'),
+                date: new Date()
+            });
+
+            this.get('controllers.application.signedInUser').then(function(user) {
+                answer.set('user', user);
+            });
+
+            answer.save().then(function(answer) {
+                controller.get('model.answers').addObject(answer);
+                controller.setProperties({
+                    answer: ''
+                });
+            });
+        }
+    }
+});
+
 App.AskQuestionController = Ember.ArrayController.extend({
     needs: ['application'],
     sortProperties: ['date'],
